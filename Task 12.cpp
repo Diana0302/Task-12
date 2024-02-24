737982ccf2e4d5d04fddcfb35396f4c2eaf2254b
Diana0302 <138877250+Diana0302@users.noreply.github.com>
2024-02-24 16:25:48 +0400

    Add files via upload

---
+++ Task 12.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+class Book {
+private: std::string title;
+	   std::string author;
+	   int ISBN;
+public: 
+	void setTitle(std::string newTitle) {
+		  title = newTitle;
+	  }
+
+	void setAuthor(std::string newAuthor) {
+		author = newAuthor;
+	}
+
+	void setISBN(int newISBN) {
+		ISBN = newISBN;
+	}
+	
+	std::string getTitle() {
+		return title;
+	}
+	std::string getAuthor() {
+		return author;
+	}
+	int getISBN() {
+		return ISBN;
+	}
+};
+
+int main()
+{
+	Book ob;
+
+	ob.setTitle("Samvel");
+	ob.setAuthor("Raffi");
+	ob.setISBN(777);
+	std::cout << "Title: " << ob.getTitle() << std::endl;
+	std::cout << "Author: " << ob.getAuthor() << std::endl;
+	std::cout << "ISBN: " << ob.getISBN() << std::endl;
+	
+	
+	return 0;
+}
+
